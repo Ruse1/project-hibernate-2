419697be4df2e2525584e395dc9b804a2ca1a4ee
RuslanVrubleuski <6628762@mail.ru>
2024-08-16 17:46:59 +0300

    Creating method of getByName in countryDao, cityDao

--- src/main/java/com/javarush/vrubleuski/dao/CityDao.java
+++ src/main/java/com/javarush/vrubleuski/dao/CityDao.java
@@ -1,11 +1,22 @@
 package com.javarush.vrubleuski.dao;
 
 import com.javarush.vrubleuski.entity.City;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import java.util.Optional;
 
 public class CityDao extends DaoBase<Short, City> {
 
     public CityDao(SessionFactory sessionFactory) {
         super(City.class, sessionFactory);
+    }
+
+    public Optional<City> getByName(String name) {
+        Session session = getSessionFactory().getCurrentSession();
+        Query<City> countryQuery = session.createQuery("SELECT c FROM City c WHERE c.name = :city", getClazz())
+                .setParameter("city", name);
+        return Optional.ofNullable(countryQuery.uniqueResult());
     }
 }
--- src/main/java/com/javarush/vrubleuski/dao/CountryDao.java
+++ src/main/java/com/javarush/vrubleuski/dao/CountryDao.java
@@ -1,11 +1,22 @@
 package com.javarush.vrubleuski.dao;
 
 import com.javarush.vrubleuski.entity.Country;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import java.util.Optional;
 
 public class CountryDao extends DaoBase<Short, Country> {
 
     public CountryDao(SessionFactory sessionFactory) {
         super(Country.class, sessionFactory);
+    }
+
+    public Optional<Country> getByName(String name) {
+        Session session = getSessionFactory().getCurrentSession();
+        Query<Country> countryQuery = session.createQuery("SELECT c FROM Country c WHERE c.name = :country", getClazz())
+                .setParameter("country", name);
+        return Optional.ofNullable(countryQuery.uniqueResult());
     }
 }
