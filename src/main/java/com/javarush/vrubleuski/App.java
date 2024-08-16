52d2ab44cbf83a27c0977dca3cd9259a5d8e0f01
RuslanVrubleuski <6628762@mail.ru>
2024-08-16 17:48:34 +0300

    Creating methods of createCustomer and initializeDaoObjects

--- src/main/java/com/javarush/vrubleuski/App.java
+++ src/main/java/com/javarush/vrubleuski/App.java
@@ -1,54 +1,120 @@
 package com.javarush.vrubleuski;
 
+import com.javarush.vrubleuski.dao.*;
 import com.javarush.vrubleuski.entity.*;
+import com.javarush.vrubleuski.session.MySessionFactory;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
-import java.util.List;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 
-/**
- * Hello world!
- */
 public class App {
+
+    private SessionFactory sessionFactory;
+    private ActorDao actorDao;
+    private AddressDao addressDao;
+    private CategoryDao categoryDao;
+    private CityDao cityDao;
+    private CountryDao countryDao;
+    private CustomerDao customerDao;
+    private FilmDao filmDao;
+    private FilmTextDao filmTextDao;
+    private InventoryDao inventoryDao;
+    private LanguageDao languageDao;
+    private PaymentDao paymentDao;
+    private RentalDao rentalDao;
+    private StaffDao staffDao;
+    private StoreDao storeDao;
+
+    public App() {
+        sessionFactory = MySessionFactory.getSessionFactory();
+        initializeDaoObjects(sessionFactory);
+    }
+
     public static void main(String[] args) {
-        try (Session session = MySessionFactory.getSessionFactory().openSession()) {
+        App app = new App();
+        Customer customer = app.createCustomer();
+        System.out.println(customer.getFirstName());
+
+    }
+
+    private Customer createCustomer() {
+        Customer newCustomer = null;
+        try (Session session = sessionFactory.getCurrentSession()) {
             session.beginTransaction();
-
             try {
-                Country country = new Country();
-                country.setName("Belarus");
+                Store store = null;
+                if (storeDao.findById((byte) 1).isPresent()) {
+                    store = storeDao.findById((byte) 1).get();
+                }
+                Country country = Country.builder()
+                        .name("Belarus")
+                        .build();
+                City city = City.builder()
+                        .name("Minsk")
+                        .country(country)
+                        .build();
+                Address address = Address.builder()
+                        .address("Kolasa, 18")
+                        .district("Moskovkii")
+                        .city(city)
+                        .postal_code("220117")
+                        .phone("375336628762")
+                        .build();
 
-                session.persist(country);
-
-                City city = new City();
-                city.setName("Minsk");
-                city.setCountry(country);
-
-                session.persist(city);
-
-                Address address = new Address();
-                address.setAddress("Beleckogo18");
-                address.setDistrict("Mockovski");
-                address.setCity(city);
-                address.setPostal_code("222222");
-                address.setPhone("6628762");
-
-                session.persist(address);
-
-                Customer customer = new Customer();
-                customer.setFirstName("Ruslan");
-                customer.setLastName("Vrubleuski");
-                customer.setEmail("6628762@mail.ru");
-                customer.setIsActive(true);
-                customer.setAddress(address);
-
-                session.persist(customer);
-
+                Customer customer = Customer.builder()
+                        .store(store)
+                        .firstName("Ruslan")
+                        .lastName("Vrubleuski")
+                        .email("6628762@mail.ru")
+                        .address(address)
+                        .isActive(true)
+                        .build();
+                if (countryDao.getByName(country.getName()).isPresent()) {
+                    country = countryDao.getByName(country.getName()).get();
+                    city.setCountry(country);
+                }
+                if (cityDao.getByName(city.getName()).isPresent()) {
+                    city = cityDao.getByName(city.getName()).get();
+                    address.setCity(city);
+                }
+                countryDao.save(country);
+                cityDao.save(city);
+                addressDao.save(address);
+                newCustomer = customerDao.save(customer);
                 session.getTransaction().commit();
             } catch (Exception e) {
                 session.getTransaction().rollback();
+                System.out.println("Rollback");
             }
+            return newCustomer;
         }
     }
 
+    private void initializeDaoObjects(SessionFactory sessionFactory) {
+        Class<App> clazzApp = App.class;
+        Field[] declaredFields = clazzApp.getDeclaredFields();
+        for (int i = 0; i < declaredFields.length; i++) {
+            Class<?> superclass = declaredFields[i].getType().getSuperclass();
+            if (superclass == DaoBase.class) {
+                Class<?> type = declaredFields[i].getType();
+                String nameObj = declaredFields[i].getName();
+                Field field = null;
+                try {
+                    field = clazzApp.getDeclaredField(nameObj);
+                    field.setAccessible(true);
+                    Constructor<?> declaredConstructor = type.getDeclaredConstructor(SessionFactory.class);
+                    Object obj = type.cast(declaredConstructor.newInstance(sessionFactory));
+                    field.set(this, obj);
+                    field.setAccessible(false);
+                } catch (NoSuchFieldException | InvocationTargetException | NoSuchMethodException |
+                         InstantiationException | IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
 }
--- src/main/java/com/javarush/vrubleuski/session/MySessionFactory.java
+++ src/main/java/com/javarush/vrubleuski/session/MySessionFactory.java
@@ -1,17 +1,18 @@
-package com.javarush.vrubleuski;
+package com.javarush.vrubleuski.session;
 
 import com.javarush.vrubleuski.converter.RatingConverter;
 import com.javarush.vrubleuski.entity.*;
+import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 
 import java.util.Properties;
 
-public class SessionFactory {
-    private static SessionFactory instance;
-    private final org.hibernate.SessionFactory sessionFactory;
+public class MySessionFactory {
+    private static MySessionFactory instance;
+    private final SessionFactory sessionFactory;
 
-    private SessionFactory() {
+    private MySessionFactory() {
         Properties properties = new Properties();
         properties.put(Environment.JAKARTA_JDBC_DRIVER, "com.p6spy.engine.spy.P6SpyDriver");
         properties.put(Environment.JAKARTA_JDBC_URL, "jdbc:p6spy:mysql://localhost:3306/movie");
@@ -19,6 +20,7 @@ public class SessionFactory {
         properties.put(Environment.JAKARTA_JDBC_USER, "root");
         properties.put(Environment.JAKARTA_JDBC_PASSWORD, "mysql");
         properties.put(Environment.HBM2DDL_AUTO, "validate");
+        properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
         properties.put(Environment.SHOW_SQL, "true");
         properties.put(Environment.HIGHLIGHT_SQL, "true");
 
@@ -42,9 +44,9 @@ public class SessionFactory {
                 .buildSessionFactory();
     }
 
-    public static org.hibernate.SessionFactory getSessionFactory() {
+    public static SessionFactory getSessionFactory() {
         if (instance == null) {
-            instance = new SessionFactory();
+            instance = new MySessionFactory();
         }
         return instance.sessionFactory;
     }
