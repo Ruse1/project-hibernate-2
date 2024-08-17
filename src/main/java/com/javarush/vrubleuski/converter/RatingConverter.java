02415a783f0355f71d50cdf75ae7ccc6125f17e9
RuslanVrubleuski <6628762@mail.ru>
2024-08-17 13:05:11 +0300

    RatingConverter was changed

--- src/main/java/com/javarush/vrubleuski/converter/RatingConverter.java
+++ src/main/java/com/javarush/vrubleuski/converter/RatingConverter.java
@@ -3,27 +3,23 @@ package com.javarush.vrubleuski.converter;
 import com.javarush.vrubleuski.entity.Rating;
 import jakarta.persistence.AttributeConverter;
 
-import java.nio.charset.StandardCharsets;
-
-public class RatingConverter implements AttributeConverter<Enum<Rating>, byte[]> {
+public class RatingConverter implements AttributeConverter<Enum<Rating>, String> {
     @Override
-    public byte[] convertToDatabaseColumn(Enum<Rating> attribute) {
+    public String convertToDatabaseColumn(Enum<Rating> attribute) {
         Rating rating = (Rating) attribute;
-        byte[] data = switch (rating) {
-            case G -> Rating.G.toString().getBytes(StandardCharsets.UTF_8);
-            case PG -> Rating.PG.toString().getBytes(StandardCharsets.UTF_8);
-            case PG13 -> Rating.PG13.toString().getBytes(StandardCharsets.UTF_8);
-            case R -> Rating.R.toString().getBytes(StandardCharsets.UTF_8);
-            case NC17 -> Rating.NC17.toString().getBytes(StandardCharsets.UTF_8);
+        String data = switch (rating) {
+            case G -> Rating.G.toString();
+            case PG -> Rating.PG.toString();
+            case PG13 -> Rating.PG13.toString();
+            case R -> Rating.R.toString();
+            case NC17 -> Rating.NC17.toString();
         };
         return data;
     }
 
     @Override
-    public Enum<Rating> convertToEntityAttribute(byte[] dbData) {
-        String strRating = new String(dbData, StandardCharsets.UTF_8);
-        Rating rating = switch (strRating) {
-            case "G" -> Rating.G;
+    public Enum<Rating> convertToEntityAttribute(String dbData) {
+        Rating rating = switch (dbData) {
             case "PG" -> Rating.PG;
             case "PG-13" -> Rating.PG13;
             case "R" -> Rating.R;
